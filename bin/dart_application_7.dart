9a2ad8280913581d34c818bb59319625cba3a449
vladpotuk <vladpotuk01@gmail.com>
2024-01-18 21:26:30 +0200

    first commit

---
+++ bin/dart_application_7.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application_7/dart_application_7.dart' as dart_application_7;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_7.calculate()}!');
+}
---
+++ test/dart_application_7_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_application_7/dart_application_7.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
